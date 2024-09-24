f41d677e7b0e1d2614536642df50649debdb10e3
Andrei Leca <15813175+HMDCrew@users.noreply.github.com>
2024-09-24 19:55:48 +0200

    remove 5px bug regression

--- themes/stonehouse-child/assets/src/js/sections/inc/items/pointMarker.js
+++ themes/stonehouse-child/assets/src/js/sections/inc/items/pointMarker.js
@@ -1,5 +1,5 @@
 export const pointMarker = () => {
     return (
-        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.1" viewBox="-12 -12 24 24" style="transform: translateY(5px)"><circle r="9" style="stroke:#fff;stroke-width:3;fill:#2A93EE;fill-opacity:1;opacity:1;animation: leaflet-control-locate-throb 4s ease infinite;"></circle></svg>'
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.1" viewBox="-12 -12 24 24"><circle r="9" style="stroke:#fff;stroke-width:3;fill:#2A93EE;fill-opacity:1;opacity:1;animation: leaflet-control-locate-throb 4s ease infinite;"></circle></svg>'
     )
 }
